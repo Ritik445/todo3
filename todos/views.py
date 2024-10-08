56a2d8b4a8f64795f69421992b06f6e2821dcc23
Ritik445 <96790035+Ritik445@users.noreply.github.com>
2024-10-08 11:56:50 +0530

    Code commit

---
+++ todos/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from .models import Todo
+from django.http import HttpResponseRedirect
+
+class IndexView(generic.ListView):
+    template_name = 'todos/index.html'
+    context_object_name = 'todo_list'
+
+    def get_queryset(self):
+        """Return all the latest todos."""
+        return Todo.objects.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        """Add additional context for the template."""
+        context = super().get_context_data(**kwargs)
+        completed_tasks_count = Todo.objects.filter(isCompleted=True).count()
+        context['completed_tasks_count'] = completed_tasks_count
+        return context
+
+def add(request):
+    title = request.POST['title']
+    Todo.objects.create(title=title)
+    return redirect('todos:index')
+
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
+    return redirect('todos:index')
+
+def update(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    isCompleted = request.POST.get('isCompleted', False)
+    if isCompleted == 'on':
+        isCompleted = True
+    todo.isCompleted = isCompleted
+    todo.save()
+    return redirect('todos:index')
